eddecebacda7a5b7f78db22a78044ac3fcd93f88
SamuelGB <samuelguardiabautista@gmail.com>
2025-06-26 05:20:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const enviarCorreoSeries = require("./EnviarCorreoSeries");
-
+// Este es un cambio mínimo para forzar redeploy
 app.use(express.json());
 app.use(enviarCorreoSeries);
 
