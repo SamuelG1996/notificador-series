beba7b3e1f7d53affd93e1a72e63dffbe85a7661
SamuelGB <samuelguardiabautista@gmail.com>
2025-06-27 10:34:34 -0500

    Update utilsSeries.js

--- utilsSeries.js
+++ utilsSeries.js
@@ -53,7 +53,7 @@ function createTablaHTMLSeries(empresa, registros) {
   }
   html += `</tr>`;
 
-  html += `</table><br><p>📅 Revisión al ${new Date().toLocaleDateString()}</p>
+  html += `</table><br><p>📅 Revisión al ${new Date().toLocaleDateString("es-ES")}</p>`;
   <p>👉 Por favor, revisa el portal para más información. Es importante que todos los registros cuenten con un estado.</p>`;
   return html;
 }
